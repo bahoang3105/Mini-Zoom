ffb6e620a121ae7bfca9bf24b3c9adeed1211b7b
bahoang3105 <73835461+bahoang3105@users.noreply.github.com>
2021-11-26 17:41:58 +0700

    Update ParticipantItem.js

--- frontend/src/component/room/participant/ParticipantItem.js
+++ frontend/src/component/room/participant/ParticipantItem.js
@@ -1,7 +1,7 @@
 const ParticipantItem = (props) => {
   return (
     <div className='participant-item'>
-      <div className='avatar' style={{ backgroundColor: `#${Math.floor(Math.random()*100)}${Math.floor(Math.random()*100)}${Math.floor(Math.random()*100)}`}}>
+      <div className='avatar' style={{ backgroundColor: `rgb(${150 + Math.floor(Math.random()*100)}, ${150 + Math.floor(Math.random()*100)}, ${150 + Math.floor(Math.random()*100)})`}}>
         {props.data.name[0].toUpperCase()}
       </div>
       <div className='participant-name'>
